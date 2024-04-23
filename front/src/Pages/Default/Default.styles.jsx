6f38416f362598976a73f9702bf0409e920d6d38
Gabriel Marcelino <gabriel.marcelinob15@gmail.com>
2024-04-23 16:12:52 -0300

    ajustado para ficar responsivo

--- front/src/Pages/Default/Default.styles.jsx
+++ front/src/Pages/Default/Default.styles.jsx
@@ -2,23 +2,45 @@ import styled from "styled-components";
 
 
 export const Rodape = styled.div`
-background-color: white;
-width: -webkit-fill-available;
-height: 40px;
-display: flex;
-position: fixed;
-bottom: 0;
-border-radius: 5px
+    background-color: white;
+    width: 100%;
+    height: 40px;
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    border-radius: 5px;
+
+    @media only screen and (min-width: 1366px) {
+        width: 1366px; /* Tamanho para HD */
+    }
+
+    @media only screen and (min-width: 1920px) {
+        width: 1920px; /* Tamanho para Full HD */
+    }
 `
 
 export const PrinciPalPanel = styled.div`
-background-color: white;
-position: absolute;
+    background-color: white;
+    position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     display: flex;
-    width: 2500px;
+    width: 100%;
     height: 800px;
     border-radius: 10px;
+
+    @media only screen and (min-width: 1366px) {
+        width: 1300px; /* Tamanho para HD */
+        height: 850px;
+    }
+
+    @media only screen and (min-width: 1920px) {
+        width: 1800px; /* Tamanho para Full HD */
+        height: 800px;
+    }
+    @media only screen and (min-width: 2560px) {
+        width: 2500px; /* Tamanho para UltraWide */
+        height: 850px;
+    }
 `
--- front/src/Pages/Login/Login.styles.jsx
+++ front/src/Pages/Login/Login.styles.jsx
@@ -5,43 +5,43 @@ export const Geral = styled.div`
 `
 
 export const Container = styled.div`
-width: 700px;
-height: auto; /* Alterado para 'auto' para permitir que a altura se ajuste conforme o conteúdo */
-padding: 30px 20px;
-background-color: white;
-border-radius: 10px;
-box-sizing: border-box;
-z-index: 999;
-text-align: center;
-box-shadow: rgb(0 0 0 / 35%) 0px 5px 5px;
-transform: translate(-50%, -50%) scale(0.5);
-transition: opacity 300ms ease-in-out, top 1000ms ease-in-out, transform 1000ms ease-in-out;
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-display: flex;
-flex-direction: column;
-justify-content: space-between;
+    width: 700px;
+    height: auto; /* Alterado para 'auto' para permitir que a altura se ajuste conforme o conteúdo */
+    padding: 30px 20px;
+    background-color: white;
+    border-radius: 10px;
+    box-sizing: border-box;
+    z-index: 999;
+    text-align: center;
+    box-shadow: rgb(0 0 0 / 35%) 0px 5px 5px;
+    transform: translate(-50%, -50%) scale(0.5);
+    transition: opacity 300ms ease-in-out, top 1000ms ease-in-out, transform 1000ms ease-in-out;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `;
 
 export const Logo = styled.div`
-width: 100px;
+    width: 100px;
 `
 
 export const IniciaLogin = styled.div`
     display: flex;
 `
 export const Campos = styled.div`
-display: flex;
-position: relative;
-box-sizing: inherit;
-justify-content: flex-end;
-margin: 5px;
+    display: flex;
+    position: relative;
+    box-sizing: inherit;
+    justify-content: flex-end;
+    margin: 5px;
 `
 export const CampoPreencher = styled.div`
-height: 25px;
-width: 300px;
+    height: 25px;
+    width: 300px;
 `
 
 export const NomeCampo = styled.div`
